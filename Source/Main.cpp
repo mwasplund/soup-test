6ad778f8947190461708745befa1547b06dc896c
Matthew Asplund <mwasplund@outlook.com>
2019-04-08 11:09:13 -0700

    Add main and program

---
+++ Source/Main.cpp
@@ -0,0 +1,19 @@
+﻿// <copyright file="Main.cpp" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+import std.core;
+import SoupSyntax;
+
+#include "Program.h"
+
+int main(int argc, char** argv)
+{
+    std::vector<std::string> args;
+    for (int i = 0; i < argc; i++)
+    {
+        args.push_back(argv[i]);
+    }
+
+    return Soup::Test::Program::Main(std::move(args));
+}
---
+++ Source/Program.h
@@ -0,0 +1,60 @@
+﻿// <copyright file="Program.h" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+#pragma once
+
+namespace Soup::Test
+{
+    /// <summary>
+    /// The root of all evil
+    /// </summary>
+    class Program
+    {
+    public:
+        /// <summary>
+        /// The main entry point of the program
+        /// </summary>
+        static int Main(std::vector<std::string> args)
+        {
+            try
+            {
+                if (args.size() != 2)
+                {
+                    throw std::runtime_error("Expected exactly one argument.");
+                }
+
+                auto& file = args[1];
+                std::cout << "Start: " << file << std::endl;
+
+                auto sourceFilePath = std::filesystem::path(file);
+                auto syntaxTree = ParseFile(sourceFilePath);
+
+                return 0;
+            }
+            catch (const std::exception& ex)
+            {
+                std::cout << "ERROR: " << ex.what() << std::endl;
+                return -1;
+            }
+        }
+
+    private:
+        static std::shared_ptr<const Soup::Syntax::SyntaxTree> ParseFile(std::filesystem::path& file)
+        {
+            std::ifstream sourceFile(file);
+
+            // Read the whole file
+            sourceFile.seekg(0, std::ios::end);
+            size_t size = sourceFile.tellg();
+            std::string source(size, ' ');
+            sourceFile.seekg(0);
+            sourceFile.read(&source[0], size); 
+
+            // Soup::Syntax::SyntaxParser::PrintAllTokens(source);
+            auto syntaxTree = Soup::Syntax::SyntaxParser::Parse(source);
+
+            return syntaxTree;
+        }
+    };
+}
